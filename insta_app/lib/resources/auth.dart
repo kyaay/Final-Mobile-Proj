45f98529df9b8cbc079f7760a4d9a5f3bae5479f
James Christian Gadiane <gadiane.jc@gmail.com>
2022-06-10 01:44:50 +0800

    Login + Register functions

--- insta_app/lib/resources/auth.dart
+++ insta_app/lib/resources/auth.dart
@@ -4,6 +4,8 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:insta_app/resources/storage.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -25,19 +27,55 @@ class AuthMethods {
         // register user
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
-        // add user to database
+
+        // image
+        String photoUrl =
+            await Storage().uploadImageToStorage('profilePics', file, false);
+
+        // UserDB
         await _firestore.collection('users').doc(cred.user!.uid).set({
           'username': username,
           'uid': cred.user!.uid,
           'email': email,
           'followers': [],
           'following': [],
+          'photoUrl': photoUrl,
         });
+
         result = "success";
       }
-    } catch (err) {
-      result = err.toString();
+    } on FirebaseAuthException catch (err) {
+      if (err.code == 'invalid-email') {
+        result = 'The email is badly formatted';
+      } else if (err.code == 'weak-password') {
+        result = 'Password should be at least 6 characters';
+      }
     }
     return result;
+  }
+
+  // User login
+  Future<String> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    String res = "Some error occurred";
+
+    try {
+      if (email.isNotEmpty && password.isNotEmpty) {
+        await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+        res = "success";
+      } else {
+        res = "Please enter username/password";
+      }
+    } on FirebaseAuthException catch (err) {
+      if (err.code == 'user-not-found') {
+        res = "This user does not exist.";
+      } else if (err.code == 'wrong-password') {
+        res = "The password is incorrect.";
+      }
+    }
+    return res;
   }
 }
