25ba2b7fd44c75ef24d9d3f46e1799d6d1d3dcea
James Christian Gadiane <gadiane.jc@gmail.com>
2022-06-15 02:25:49 +0800

    pubspec + main

--- insta_app/lib/main.dart
+++ insta_app/lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'src/app.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+void main() async {
   runApp(const MyApp());
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.openBox('accounts');
   await Hive.openBox('todos');
   await Hive.initFlutter();
-void main() async {
 }
