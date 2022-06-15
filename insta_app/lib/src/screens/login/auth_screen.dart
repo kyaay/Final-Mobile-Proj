383f2ad202d6d46eccce9000c09c3d2932b2f385
Khiara Danielle Rubia <khiara.rubia@usjr.edu.ph>
2022-06-16 01:03:20 +0800

    Login screen edit + errors
    Edited the background, pero naa shay error katung inig click sa register wala ko kasabot

--- insta_app/lib/src/screens/login/auth_screen.dart
+++ insta_app/lib/src/screens/login/auth_screen.dart
@@ -12,14 +12,14 @@ class Wrapper extends StatelessWidget {
           if (_authController.currentUser == null) {
             return AuthScreen(_authController);
           } else {
-            return TodoScreen(_authController);
+            return InstaScreen(_authController);
           }
         });
   }
 }
 
-Widget TodoScreen(AuthController authController) {
-  return TodoScreen(authController);
+Widget InstaScreen(AuthController authController) {
+  return InstaScreen(authController);
 }
 
 class AuthScreen extends StatefulWidget {
@@ -42,110 +42,124 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        backgroundColor: const Color(0xFF303030),
-      ),
-      backgroundColor: Colors.grey[400],
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              child: Form(
-                key: _formKey,
-                onChanged: () {
-                  _formKey.currentState?.validate();
-                  if (mounted) {
-                    setState(() {});
-                  }
-                },
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Text(prompts),
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(hintText: 'Username'),
-                      controller: _unCon,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your username';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        hintText: 'Password',
-                      ),
-                      controller: _passCon,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your password';
-                        }
-                        return null;
-                      },
-                    ),
-                    Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 32),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            ElevatedButton(
-                              onPressed:
-                                  (_formKey.currentState?.validate() ?? false)
-                                      ? () {
-                                          String result = _auth.register(
-                                              _unCon.text, _passCon.text);
-                                          setState(() {
-                                            prompts = result;
-                                          });
-                                        }
-                                      : null,
-                              style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(16.0),
-                                  ),
-                                  primary: (_formKey.currentState?.validate() ??
-                                          false)
-                                      ? const Color(0xFF303030)
-                                      : Colors.grey),
-                              child: const Text('Register'),
-                            ),
-                            ElevatedButton(
-                              onPressed:
-                                  (_formKey.currentState?.validate() ?? false)
-                                      ? () {
-                                          bool result = _auth.login(
-                                              _unCon.text, _passCon.text);
-                                          if (!result) {
-                                            setState(() {
-                                              prompts =
-                                                  'Error logging in, username or password may be incorrect or the user has not been registered yet.';
-                                            });
-                                          }
-                                        }
-                                      : null,
-                              style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(16.0),
-                                  ),
-                                  primary: (_formKey.currentState?.validate() ??
-                                          false)
-                                      ? const Color(0xFF303030)
-                                      : Colors.grey),
-                              child: const Text('Log in'),
-                            ),
-                          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Form(
+                  key: _formKey,
+                  onChanged: () {
+                    _formKey.currentState?.validate();
+                    if (mounted) {
+                      setState(() {});
+                    }
+                  },
+                  child: Container(
+                    color: Colors.grey.withOpacity(.8),
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Text(prompts),
                         ),
-                      ),
-                    )
-                  ],
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(hintText: 'Username'),
+                          controller: _unCon,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your username';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            hintText: 'Password',
+                          ),
+                          controller: _passCon,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          },
+                        ),
+                        Flexible(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 32),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                ElevatedButton(
+                                  onPressed:
+                                      (_formKey.currentState?.validate() ??
+                                              false)
+                                          ? () {
+                                              String result = _auth.register(
+                                                  _unCon.text, _passCon.text);
+                                              setState(() {
+                                                prompts = result;
+                                              });
+                                            }
+                                          : null,
+                                  style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(16.0),
+                                      ),
+                                      primary:
+                                          (_formKey.currentState?.validate() ??
+                                                  false)
+                                              ? const Color(0xFF303030)
+                                              : Colors.grey),
+                                  child: const Text('Register'),
+                                ),
+                                ElevatedButton(
+                                  onPressed:
+                                      (_formKey.currentState?.validate() ??
+                                              false)
+                                          ? () {
+                                              bool result = _auth.login(
+                                                  _unCon.text, _passCon.text);
+                                              if (!result) {
+                                                setState(() {
+                                                  prompts =
+                                                      'Error logging in, username or password may be incorrect or the user has not been registered yet.';
+                                                });
+                                              }
+                                            }
+                                          : null,
+                                  style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(16.0),
+                                      ),
+                                      primary:
+                                          (_formKey.currentState?.validate() ??
+                                                  false)
+                                              ? const Color(0xFF303030)
+                                              : Colors.grey),
+                                  child: const Text('Log in'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
